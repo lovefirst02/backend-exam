25b4bdb3a3ddc293eb38c6f3257f62466c7e0f61
Hao <>
2025-12-15 23:05:44 +0800

    feat: implement swap function with pointer validation and reflection

--- swap/main.go
+++ swap/main.go
@@ -2,9 +2,27 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 )
 
-func swap[T any](a, b T) {}
+func swap[T any](a, b T) {
+
+	a_reflect := reflect.ValueOf(a)
+	b_reflect := reflect.ValueOf(b)
+
+	if a_reflect.Kind() != reflect.Ptr || b_reflect.Kind() != reflect.Ptr {
+		panic("swap: both arguments must be pointers (call swap(&a, &b))")
+	}
+
+	if a_reflect.Type() != b_reflect.Type() {
+		panic("swap: pointer types must be identical")
+	}
+
+	tmp_reflect := reflect.New(a_reflect.Elem().Type()).Elem()
+	tmp_reflect.Set(a_reflect.Elem())
+	a_reflect.Elem().Set(b_reflect.Elem())
+	b_reflect.Elem().Set(tmp_reflect)
+}
 
 func main() {
 	a := 10
