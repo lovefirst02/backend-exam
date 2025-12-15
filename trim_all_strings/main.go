e97407d465949b2f0a6125aa8e39bfd9768dc370
Hao <>
2025-12-15 23:35:46 +0800

    feat: implement TrimAllStrings function to recursively trim whitespace from all string fields

--- trim_all_strings/main.go
+++ trim_all_strings/main.go
@@ -3,9 +3,43 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"reflect"
+	"strings"
 )
 
-func TrimAllStrings(a any) {}
+func TrimAllStrings(a any) {
+	visited := make(map[uintptr]bool)
+
+	var trim func(v reflect.Value)
+	trim = func(v reflect.Value) {
+		if !v.IsValid() {
+			return
+		}
+
+		switch v.Kind() {
+		case reflect.Ptr:
+			ptr := v.Pointer()
+			if ptr != 0 {
+				if visited[ptr] {
+					return
+				}
+				visited[ptr] = true
+			}
+			trim(v.Elem())
+		case reflect.Interface:
+			trim(v.Elem())
+		case reflect.Struct:
+			for i := 0; i < v.NumField(); i++ {
+				trim(v.Field(i))
+			}
+		case reflect.String:
+			v.SetString(strings.TrimSpace(v.String()))
+		}
+	}
+
+	trim(reflect.ValueOf(a))
+
+}
 
 func main() {
 	type Person struct {
